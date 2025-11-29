4383ffe91d8848b258bd7f48ea332ae6d3e1984f
Monica Mateescu <monica.antonie@gmail.com>
2025-11-29 22:41:21 +0100

    form validation

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import Button from "./Button";
 
 const EntryForm = ({ setOpen }) => {
+  const [form, setForm] = useState({
+    title: "",
+    date: "",
+    image: "",
+    content: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    const { title, date, image, content } = form;
+
+    if (!title.trim() || !date.trim() || !image.trim() || !content.trim())
+      return;
+
     setOpen(false);
   };
+
+  const disabled =
+    !form.title.trim() ||
+    !form.date.trim() ||
+    !form.image.trim() ||
+    !form.content.trim();
+
   return (
     <>
       <h3 className="font-bold text-lg text-center mb-4">
@@ -20,27 +44,43 @@ const EntryForm = ({ setOpen }) => {
           <input
             type="text"
             name="title"
+            value={form.title}
+            onChange={handleChange}
             className="w-full input"
             placeholder="Title Entry"
           />
           <legend className="fieldset-legend">Date</legend>
-          <input type="date" name="date" className="w-full input" />
+          <input
+            type="date"
+            name="date"
+            value={form.date}
+            onChange={handleChange}
+            className="w-full input"
+          />
           <legend className="fieldset-legend">Image URL</legend>
           <input
-            type="tex"
+            type="url"
             name="image"
+            value={form.image}
+            onChange={handleChange}
             className="w-full input"
-            placeholder="Add the URL here"
+            placeholder="https://"
           />
 
           <legend className="fieldset-legend">Content</legend>
           <textarea
             name="content"
+            value={form.content}
+            onChange={handleChange}
             className="w-full textarea h-24"
             placeholder="How was your day?"
           ></textarea>
         </fieldset>
-        <Button label="Save Entry" className="btn-primary w-full" />
+        <Button
+          label="Save Entry"
+          className="btn-primary w-full"
+          disabled={disabled}
+        />
       </form>
     </>
   );
